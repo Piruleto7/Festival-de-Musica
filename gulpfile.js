145f8836d5dae0ee3c17349bdba6b37c9e71974a
Jairo Vanegas Paternina <128342701+Piruleto7@users.noreply.github.com>
2024-08-04 20:10:29 -0500

    archivos del proyecto Festival de Musical
    Realizados en HTML, CSS y JavaScript en Node.Js y Sass

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+import { src, dest, watch, series } from 'gulp'
+
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function js (done) {
+   src('src/js/app.js')
+   .pipe(dest('build/js'))
+   
+    done()
+}
+
+export function css(done) {
+    src('src/scss/app.scss',{sourceMap:true})
+    .pipe(sass().on('error',sass.logError))
+    .pipe(dest('build/css', {sourceMap:true}))
+
+    done()
+}
+
+export function dev() {
+    watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
+  
+}
+
+export default series(js, css, dev)
